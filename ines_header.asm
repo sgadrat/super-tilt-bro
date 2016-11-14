c47ff3edfec74a5127010956c3b760550bb5765f
Sylvain Gadrat <sgadrat@wontfix.it>
2016-11-14 17:43:34 +0100

    Set TV system to PAL

--- ines_header.asm
+++ ines_header.asm
@@ -1,6 +1,13 @@
-.asc "NES", $1A         ;iNES magic
-.byt 1                  ;PRG section occupies 1*16KiB memory
-.byt 1                  ;CHR section occupies 1* 8KiB memory
-.byt %00000000          ;ROM options - horizontal mirroring
-.byt 0                  ;mapper #0 (no mapper)
-.dsb 8,0                ;trailing zeros
+.asc "NES", $1A ; iNES magic
+.byt 1          ; PRG section occupies 1*16KiB memory
+.byt 1          ; CHR section occupies 1* 8KiB memory
+.byt %00000000  ; Flags 6 - mapper 0, horizontal mirroring, no trainer, no persistent memory
+.byt 0          ; Flags 7 - mapper 0, not NES 2.0, not PlayChoice10, not VS unisystem
+.byt 0          ; Size of PRG-RAM
+.byt %00000001  ; Flags 9 - PAL
+.byt 0          ;
+.byt 0          ;
+.byt 0          ; Unused in iNES
+.byt 0          ;
+.byt 0          ;
+.byt 0          ;
