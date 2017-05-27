7faf81e09a2d5904a1e3cb37f0224fcb2dcb663c
Sylvain Gadrat <sgadrat@wontfix.it>
2017-05-27 10:54:04 +0200

    Fix unwanted hitstun after death

--- prg_rom/game.asm
+++ prg_rom/game.asm
@@ -714,9 +714,9 @@ set_death_state:
 jsr audio_play_death ; Play death sound
 lda #$00                         ; Reset aerial jumps counter
 sta player_a_num_aerial_jumps, x ;
+sta player_a_hitstun, x ; Reset hitstun counter
 lda #DEFAULT_GRAVITY     ; Reset gravity
 sta player_a_gravity, x  ;
-sta player_a_hitstun, x ; Reset hitstun counter
 dec player_a_stocks, x ; Decrement stocks counter and check for gameover
 bmi gameover           ;
 jsr particle_death_start ; Death particles animation
