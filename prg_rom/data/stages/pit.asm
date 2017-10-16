c75bf9d972dd2c6782f26202e027978c15852225
Sylvain Gadrat <sgadrat@wontfix.it>
2017-10-16 12:48:42 +0200

    Redesign The Pit

--- prg_rom/data/stages/pit.asm
+++ prg_rom/data/stages/pit.asm
@@ -1,11 +1,8 @@
 stage_pit_data:
-STAGE_HEADER($4000, $bc00, $90ff, $90ff, $7cbc, $3800) ; player_a_x, player_b_x, player_a_y, player_b_y, respawn_x, respawn_y
+STAGE_HEADER($1800, $e000, $80ff, $80ff, $1800, $5800) ; player_a_x, player_b_x, player_a_y, player_b_y, respawn_x, respawn_y
 stage_pit_platforms:
-PLATFORM($69, $8f, $41, $5f) ; top
-PLATFORM($09, $2f, $81, $ff) ; left 3
-PLATFORM($29, $4f, $91, $ff) ; left 2
-PLATFORM($49, $5f, $a1, $ff) ; left 1
-PLATFORM($99, $af, $a1, $ff) ; right 1
-PLATFORM($a9, $cf, $91, $ff) ; right 2
-PLATFORM($c9, $ef, $81, $ff) ; right 3
+PLATFORM($01, $3f, $81, $ff) ; left, right, top, bot
+PLATFORM($b9, $fe, $81, $ff) ; left, right, top, bot
+SMOOTH_PLATFORM($49, $6f, $93) ; left, right, top
+SMOOTH_PLATFORM($89, $af, $b3) ; left, right, top
 END_OF_STAGE
