293d85fce665da7b0807b65b7d051efda7f89dac
Sylvain Gadrat <sgadrat@wontfix.it>
2018-05-08 17:37:59 +0200

    stblib: Allow to parse hexadecimal in asm tiles

--- tools/stblib/tiles.py
+++ tools/stblib/tiles.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 import re
 
-tile_byte = '%([01])([01])([01])([01])([01])([01])([01])([01])'
+tile_byte = '(%|\$)(([01][01][01][01][01][01][01][01])|([0-9a-f][0-9a-f]))'
 re_tileline = re.compile('\.byt %s, %s, %s, %s, %s, %s, %s, %s' % ((tile_byte,)*8))
 
 class Tile:
@@ -33,9 +33,28 @@ class Tile:
 				raise Exception('Too much tile lines')
 
 			for byte in range(8):
+				# Convert the byte from source line to a list of bits
+				bits = []
+				format_group_index = 1 + byte*4
+				format_designer = m.group(format_group_index)
+				byte_representation = m.group(format_group_index+1)
+
+				bin_str = '00000000'
+				if format_designer == '%':
+					bin_str = byte_representation
+				elif format_designer == '$':
+					bin_str = bin(int(byte_representation, 16))[2:].zfill(8)
+				else:
+					assert False
+
+				for bit_chr in bin_str:
+					assert bit_chr == '0' or bit_chr == '1'
+					bits.append(int(bit_chr))
+				assert len(bits) == 8
+
+				# Update representation with the current byte
 				for bit in range(8):
-					group_index = 1 + byte*8 + bit
-					self._representation[byte][bit] += int(m.group(group_index)) * self._asm_line_multiplier
+					self._representation[byte][bit] += bits[bit] * self._asm_line_multiplier
 
 	def is_complete(self):
 		return self._asm_line_multiplier == 2
