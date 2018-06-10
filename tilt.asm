3000955c3b9cd5f9124ad9c57cc2937c4d3b5c53
Sylvain Gadrat <sgadrat@wontfix.it>
2018-06-10 10:33:34 +0200

    build: Allow to build raw ROMs

--- tilt.asm
+++ tilt.asm
@@ -1,10 +1,22 @@
 ; Building the project
-;  xa tilt.asm -C -o tilt.nes
+;  xa tilt.asm -C -o tilt\(E\).nes
+;
+; Building raw ROMs
+;  xa tilt.asm -DNO_INES_HEADER -DNO_CHR_ROM -C -o tilt_prg.bin
+;  xa tilt.asm -DNO_INES_HEADER -DNO_PRG_ROM -C -o tilt_chr.bin
 
+#ifndef NO_INES_HEADER
 #include "ines_header.asm"
+#endif
+
+#ifndef NO_PRG_ROM
 #include "constants.asm"
 #include "macros.asm"
 #include "nes_labels.asm"
 #include "mem_labels.asm"
 #include "prg_rom/prg_rom.asm"
+#endif
+
+#ifndef NO_CHR_ROM
 #include "chr_rom.asm"
+#endif
