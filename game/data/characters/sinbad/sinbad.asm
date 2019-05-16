3f6db543e12002bbc5018f04d5c035321cbf5efa
Sylvain Gadrat <sgadrat@wontfix.it>
2019-05-16 23:04:52 +0200

    WIP(multi-char) build: put sinbad-specific tiles in its character folder

--- game/data/characters/sinbad/sinbad.asm
+++ game/data/characters/sinbad/sinbad.asm
@@ -1,5 +1,6 @@
 SINBAD_BANK_NUMBER = CURRENT_BANK_NUMBER
 
+#include "game/data/characters/sinbad/chr_tiles.asm"
 #include "game/data/characters/sinbad/animations/animations.asm"
 #include "game/data/characters/sinbad/character_colors.asm"
 #include "game/data/characters/sinbad/state_events.asm"
