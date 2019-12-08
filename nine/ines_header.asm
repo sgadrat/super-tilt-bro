4e9be263b0478fb703d20502a484613e472e61ea
Sylvain Gadrat <sgadrat@wontfix.it>
2019-12-09 00:15:29 +0100

    iNES header: mapper 0xF1E

--- nine/ines_header.asm
+++ nine/ines_header.asm
@@ -1,13 +1,13 @@
 .asc "NES", $1A ; iNES magic
 .byt 32         ; PRG section occupies 32*16KiB memory
 .byt 0          ; CHR-ROM section occupies 0*8KiB memory (we use CHR-RAM)
-.byt %11100010  ; Flags 6 - mapper 30, Horizontal mirroring, no trainer, persistent memory
-.byt %00010000  ; Flags 7 - mapper 30, not NES 2.0, not PlayChoice10, not VS unisystem
-.byt 0          ; Size of PRG-RAM
-.byt %00000001  ; Flags 9 - PAL
-.byt 0          ;
-.byt 0          ;
-.byt 0          ; Unused in iNES
-.byt 0          ;
-.byt 0          ;
-.byt 0          ;
+.byt %11100010  ; Flags 6 NNNN FTBM - mapper 3870, no four screen, no trainer, persistent memory, horizontal mirroring
+.byt %00011000  ; Flags 7 NNNN 10TT - mapper 3870, NES 2.0, NES/Famicom
+.byt %00001111  ; Flags 8 SSSS NNNN - submapper 0, mapper 3870
+.byt %00000000  ; Flags 9 CCCC PPPP - CHR-ROM size MSB = 0, PRG-ROM size MSB = 0
+.byt 0          ; Flags 10
+.byt %00000111 ; Flags 11 cccc CCCC - CHR-NVRAM, CHR-RAM
+.byt %00000001  ; Flags 12 .... ..VV - PAL timing
+.byt 0          ; Flags 13
+.byt 0          ; Flags 14
+.byt 0          ; Flags 15
