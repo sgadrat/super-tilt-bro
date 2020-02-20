364d1b48663f5a286a21c83bbb24fd2077aa1bf5
Sylvain Gadrat <sgadrat@wontfix.it>
2020-02-20 22:37:40 +0100

    menus: improve scrolling transition
     * Takes less frames to complete (to waste less time between menus)
     * Features acceleration, deceleration and bouncing (to feel more powerful)

--- game/logic/game_states/transitions/scroll_transition.asm
+++ game/logic/game_states/transitions/scroll_transition.asm
@@ -17,7 +17,7 @@ state_transition_pre_scroll_down:
 	rts
 
 	camera_steps:
-	.byt 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, $ff
+	.byt 10, 20, 40, 60, 100, 140, 180, 220, 240, $ff
 .)
 
 state_transition_pre_scroll_up:
@@ -39,7 +39,7 @@ state_transition_pre_scroll_up:
 	rts
 
 	camera_steps:
-	.byt 230, 220, 210, 200, 190, 180, 170, 160, 150, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0, $ff
+	.byt 230, 220, 200, 180, 140, 100, 60, 20, 0, $ff
 .)
 
 state_transition_post_scroll_down:
@@ -67,7 +67,7 @@ state_transition_post_scroll_down:
 	rts
 
 	camera_steps:
-	.byt 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240, $ff
+	.byt 40, 80, 120, 160, 200, 240, 235, 239, 230, 239, 237, 235, 240, $ff
 .)
 
 state_transition_post_scroll_up:
@@ -90,7 +90,7 @@ state_transition_post_scroll_up:
 	rts
 
 	camera_steps:
-	.byt 230, 220, 210, 200, 190, 180, 170, 160, 150, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10, 0, $ff
+	.byt 200, 160, 140, 120, 106, 93, 80, 67, 54, 41, 28, 15, 0, $ff
 .)
 
 ; Transition by scrolling between two screen, keeping menus clouds
@@ -123,7 +123,7 @@ scroll_transition:
 			sta screen_sprites_offset_lsb
 			lda #0
 			sta screen_sprites_offset_msb
-			lda #$fd
+			lda #$fa
 			pha      ; cloud_scroll_lsb
 			lda #$ff
 			pha      ; cloud_scroll_msb
@@ -140,7 +140,7 @@ scroll_transition:
 			sta screen_sprites_offset_lsb
 			lda #$ff
 			sta screen_sprites_offset_msb
-			lda #$3
+			lda #$6
 			pha      ; cloud_scroll_lsb
 			lda #$0
 			pha      ; cloud_scroll_msb
