f75d4079aa43645f223ca992b9b6d231c4bf6f1f
Sylvain Gadrat <sgadrat@wontfix.it>
2020-03-21 17:46:21 +0100

    network: avoid network labels clashing with tmpfields

--- game/mem_labels.asm
+++ game/mem_labels.asm
@@ -182,18 +182,18 @@ extra_tmpfield6 = $b5
 ; Network engine labels
 ;
 
-network_current_frame_byte0 = $b2
-network_current_frame_byte1 = $b3
-network_current_frame_byte2 = $b4
-network_current_frame_byte3 = $b5
+network_current_frame_byte0 = $b6
+network_current_frame_byte1 = $b7
+network_current_frame_byte2 = $b8
+network_current_frame_byte3 = $b9
 
-network_client_id_byte0 = $b6
-network_client_id_byte1 = $b7
-network_client_id_byte2 = $b8
-network_client_id_byte3 = $b9
+network_client_id_byte0 = $ba
+network_client_id_byte1 = $bb
+network_client_id_byte2 = $bc
+network_client_id_byte3 = $bd
 
-network_last_sent_btns = $ba
-network_last_received_btns = $bb
+network_last_sent_btns = $be
+network_last_received_btns = $bf
 
 ;
 ; TITLE labels
