dacb5c50d9f04ba658d0fbd0c0fc66a0cd481594
Sylvain Gadrat <sgadrat@wontfix.it>
2020-05-01 11:53:37 +0200

    menus: re-enable access to local mode
    Local mode is still broken. This is just a revert to allow working on it.
    
    This reverts commit a97f8ec8ba506746cd922329cf480700a860e0ab.

--- game/logic/game_states/game_states.asm
+++ game/logic/game_states/game_states.asm
@@ -12,7 +12,6 @@ VECTOR(init_netplay_launch_screen)
 VECTOR(init_donation_screen)
 VECTOR(init_donation_btc_screen)
 VECTOR(init_donation_paypal_screen)
-VECTOR(init_no_local_screen)
 
 ; Subroutine called each frame
 game_states_tick:
@@ -28,7 +27,6 @@ VECTOR(netplay_launch_screen_tick)
 VECTOR(donation_screen_tick)
 VECTOR(donation_qr_screen_tick)
 VECTOR(donation_qr_screen_tick)
-VECTOR(no_local_screen_tick)
 
 #define GAME_STATE_INGAME $00
 #define GAME_STATE_TITLE $01
@@ -42,7 +40,6 @@ VECTOR(no_local_screen_tick)
 #define GAME_STATE_DONATION $09
 #define GAME_STATE_DONATION_BTC $0a
 #define GAME_STATE_DONATION_PAYPAL $0b
-#define GAME_STATE_NO_LOCAL $0c
 ;NOTE maximum supported value is $0f, because get_transition_id returns an ID on one byte. To handle more than 16 states, it should be changed.
 
 #include "game/logic/game_states/character_selection_screen.asm"
@@ -54,7 +51,6 @@ VECTOR(no_local_screen_tick)
 #include "game/logic/game_states/gameover_screen.asm"
 #include "game/logic/game_states/mode_selection_screen.asm"
 #include "game/logic/game_states/netplay_launch_screen.asm"
-#include "game/logic/game_states/no_local_screen.asm"
 #include "game/logic/game_states/stage_selection_screen.asm"
 #include "game/logic/game_states/title_screen.asm"
 #include "game/logic/game_states/transitions/transitions.asm"
