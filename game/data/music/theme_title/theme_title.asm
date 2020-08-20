0b319bbfc0bc2c31f6cc0ecf927fc3beb25b213f
Sylvain Gadrat <sgadrat@wontfix.it>
2020-08-20 11:52:44 +0200

    WIP(audio) flattened

--- game/data/music/theme_title/theme_title.asm
+++ game/data/music/theme_title/theme_title.asm
@@ -4,7 +4,6 @@ track_title_info:
 .word track_title_square1
 
 track_title_square1:
-.word sample_9
 .word sample_1
 .word sample_2
 .word sample_3
@@ -13,13 +12,13 @@ track_title_square1:
 .word sample_6
 .word sample_7
 .word sample_8
+.word sample_9
 .word sample_10
 .word sample_11
 .word sample_12
-.word sample_14
 .word sample_13
+.word sample_14
 .word sample_15
-;.word sample_16 ; TODO ftm converter should prune unused samples
+.word sample_16
 .word sample_17
-.word sample_18
 MUSIC_END
