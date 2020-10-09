694c8ff12360cc7da54337b003714713d91a5253
Sylvain Gadrat <sgadrat@wontfix.it>
2020-10-09 23:49:37 +0200

    collision: note random optimization ideas

--- game/logic/game_states/game/game_logic.asm
+++ game/logic/game_states/game/game_logic.asm
@@ -1030,6 +1030,23 @@ move_player:
 		;  To mitigate it more elegantly we may
 		;   - recheck only platforms before the last colliding platform (and do it until no platform collide)
 		;   - use a better formula for collision detection that does not let player cut the corners
+		;   - better formula idea (kudos darry.revok): move vertically, check vertical collisions, move horizontally, check H collisions
+		;      - Can still cut corners (one way)
+		;      - Should not be able to be trapped by multi-platform bug described above
+		;      - Still iterate two times over objects, but each time checking less collisions (only one line per platform needed)
+		;        - Maybe improvable by computing full movement, and interating once, but considering (old_x, new_y) and (old_y, new_x) on each platform
+		;          - Save one iteration (same number of collision check) at the cost of complexity
+		;          - Possibly incompatible with slopes (to be determined, not yet needed)
+		;          - thinking about it, it is dumb
+		;            - some vertical positions will be checked without the final horz position
+		;              - (multiple platform) bugs may appear
+		;                - player[0;0] momentum[2;2] platform(upleft=[1;1],botright=[3;3]
+		;                  - player could go to [0;2] and [2;0], dumb application of this formula would state that it is ok to go to [2;2]
+		;                  - could be fixed for single platform by updating one pos before computing the other
+		;                  - multi platform fix is the same: fully compute one component before considering the other
+		;                    - that means "do not use this trick, it is dumb"
+		;      - Better optimization: do not store boxes, but just lines, make lines iterable byt type (up, down, left or right)
+		;        - This is a big change, impacting engine and tools
 		;
 		; TODO implement a cleaner solution
 		lda #<handle_one_platform
