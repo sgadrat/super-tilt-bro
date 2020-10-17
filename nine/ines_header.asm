5e293bc5c9e6aa73f10a269be911c391c33f0c66
Sylvain Gadrat <sgadrat@wontfix.it>
2020-10-17 13:53:17 +0200

    build: no persistent memory on rainbow mapper
    This caused the entire PRG-ROM to be saved (since it is flashable.)
    In turn, that caused new built ROMs to load old PRG at boot.

--- nine/ines_header.asm
+++ nine/ines_header.asm
@@ -1,7 +1,7 @@
 .asc "NES", $1A ; iNES magic
 .byt 32         ; PRG section occupies 32*16KiB memory
 .byt 0          ; CHR-ROM section occupies 0*8KiB memory (we use CHR-RAM)
-.byt ((MAPPER_NUMBER & $00f) << 4) + %00000010 ; Flags 6 NNNN FTBM - mapper low nibble, no four screen, no trainer, persistent memory, horizontal mirroring
+.byt ((MAPPER_NUMBER & $00f) << 4) + (MAPPER_BATTERY_FLAG << 1) ; Flags 6 NNNN FTBM - mapper low nibble, no four screen, no trainer, persistent memory, horizontal mirroring
 .byt (MAPPER_NUMBER & $0f0) + %00001000 ; Flags 7 NNNN 10TT - mapper mid nibble, NES 2.0, NES/Famicom
 .byt (SUBMAPPER_NUMBER << 4) + ((MAPPER_NUMBER & $f00) >> 8) ; Flags 8 SSSS NNNN - submapper, mapper high nibble
 .byt %00000000  ; Flags 9 CCCC PPPP - CHR-ROM size MSB = 0, PRG-ROM size MSB = 0
