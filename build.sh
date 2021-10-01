79216dc7f35339cae3cc7d627ad4e2796e47efed
Sylvain Gadrat <sgadrat@wontfix.it>
2021-10-01 14:46:06 +0200

    build: update static bank hash
    Broke by updating to cart v1.3 at f99dbd0e23143b1aa37bef26da3f15dc4e2f6925

--- build.sh
+++ build.sh
@@ -131,7 +131,7 @@ say "======================="
 #  Static bank cannot be safely upgraded, so avoid modifying it
 #  Only a warning for ease of development, should be an error when carts are distributed to non-technical players.
 static_bank_hash=`tail -c 4096 'Super_Tilt_Bro_(E).nes' | md5sum - | grep -Eo '^[0-9a-f]+'`
-if [ "$static_bank_hash" != '5e39295241a9b1597abf642ba012ac20' ]; then
+if [ "$static_bank_hash" != '802d183b49de0361ed6b328ad8c86ef4' ]; then
 	say "WARNING: static bank changed"
 	say "======================="
 fi
