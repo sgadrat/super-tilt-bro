815ffda2da3baa85d1e857d5b42e6665feb087a3
Sylvain Gadrat <sgadrat@wontfix.it>
2021-10-07 14:58:09 +0200

    WIP(flame) tools: Optimize address_translate (also produce shorter output files)

--- tools/flame/address_translate.py
+++ tools/flame/address_translate.py
@@ -9,14 +9,29 @@ in_filename = '/tmp/nes.perf'
 
 # Get routines addresses
 routines = json.load(sys.stdin)
+routines_dict = {x['begin']: x['name'] for x in routines}
+#routines_dict = {}
+#for addr_int in range(0x10000):
+#	for routine in routines:
+#		if addr_int >= routine['begin'] and addr_int < routine['end']:
+#			routines_dict[addr_int] = routine['name']
 
 # Utilities
 def translate(addr):
-	global routines
+	global routines, routines_dict
 
 	addr_int = int(addr, 16)
+
+	# Note:
+	#  this is an ugly speedup using cache of previous results,
+	#  would be better preprocess "routines" once for all to construct
+	#  the ideal memory structure.
+	if addr_int in routines_dict:
+		return routines_dict[addr_int]
+
 	for routine in routines:
 		if addr_int >= routine['begin'] and addr_int < routine['end']:
+			routines_dict[addr_int] = routine['name']
 			return routine['name']
 
 	return '({})'.format(addr)
@@ -27,6 +42,7 @@ in_file_size = os.stat(in_filename).st_size
 read_size = 0
 begin = time.time()
 last_progress = begin
+cycles_counters = {}
 with open(in_filename, 'r') as in_file:
 	for line in in_file:
 		# Update progress
@@ -50,7 +66,12 @@ with open(in_filename, 'r') as in_file:
 		m = re_in_perf.match(line)
 		assert m is not None, "invalid line in input file: '{}'".format(line) #TODO should be ensure
 		stack = m.group('stack')
-		cycles = m.group('cycles')
+		instr = m.group('instr')
+		cycles = int(m.group('cycles'))
+		#stack, _, cycles = line.partition(' ')
+		#instr = stack[-4:]
+		#stack = stack[:-4]
+		#cycles = int(cycles)
 
 		if stack == '':
 			stack = ['top_level']
@@ -61,10 +82,11 @@ with open(in_filename, 'r') as in_file:
 			stack = stack.split(';')
 
 			# Add current instruction at the end of the stack list (see comment about prunning it below)
-			stack.append(m.group('instr'))
+			stack.append(instr)
 
 			# Resolve stack frames to routine name
 			for i in range(len(stack)):
+				#stack[i] = routines_dict.get(int(stack[i],16), '({})'.format(stack[i]))
 				stack[i] = translate(stack[i])
 
 			# Keep the last element only if the instruction is not in the routine pointed by last stack frame
@@ -72,4 +94,11 @@ with open(in_filename, 'r') as in_file:
 			if stack[-1] == stack[-2]:
 				stack = stack[:-1]
 
-		print('{} {}'.format(';'.join(stack), cycles))
+		frame_desc = ';'.join(stack)
+		cycles_counters[frame_desc] = cycles_counters.get(frame_desc, 0) + cycles
+
+if last_progress != begin:
+	sys.stderr.write('\n')
+
+for frame_desc in cycles_counters:
+	print('{} {}'.format(frame_desc, cycles_counters[frame_desc]))
