3ecd3eda4cc9ae7d6084f867d4fec60528365eb6
Sylvain Gadrat <sgadrat@wontfix.it>
2021-12-26 01:55:31 +0100

    ftm-to-stb: debugtools function to print uctf

--- tools/ftm-to-stb/debugtools.py
+++ tools/ftm-to-stb/debugtools.py
@@ -1,3 +1,5 @@
+import ftmmanip
+
 def human_readable_track(track):
 	serialized = ''
 	for pattern_idx in range(len(track['patterns'])):
@@ -19,6 +21,27 @@ def human_readable_track(track):
 					for extra_effect in channel.get('extra_effects', []):
 						serialized += ' {}={}'.format(extra_effect['effect'], extra_effect['value'])
 			serialized += '\n'
+		serialized += '\n'
+
+	return serialized
+
+def human_readable_uctf(uctf):
+	serialized = ''
+	for channel_idx in range(len(uctf['channels'])):
+		serialized += 'CHANNEL {:02X}\n'.format(channel_idx)
+		serialized += ', '.join(['{:02X}'.format(x) for x in uctf['channels'][channel_idx]])
+		serialized += '\n\n'
+
+	for sample_idx in range(len(uctf['samples'])):
+		sample = uctf['samples'][sample_idx]
+		serialized += 'SAMPLE {:02X} ({})\n'.format(sample_idx, sample['type'])
+		serialized += '# {}\n'.format('\t'.join(ftmmanip.uctf_fields_by_type[sample['type']]))
+		for line_idx in range(len(sample['lines'])):
+			line = sample['lines'][line_idx]
+			values = []
+			for field in ftmmanip.uctf_fields_by_type[sample['type']]:
+				values.append(str(line[field]) if line[field] is not None else '...')
+			serialized += '{:02X} : {}\n'.format(line_idx, '\t'.join(values))
 		serialized += '\n'
 
 	return serialized
