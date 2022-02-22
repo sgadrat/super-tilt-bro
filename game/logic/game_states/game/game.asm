1a1c344200a726043dddb29d59dd97bf28f77d47
Sylvain Gadrat <sgadrat@wontfix.it>
2022-02-22 23:10:19 +0100

    cleaning: clear NETWORK_AI leftovers

--- game/logic/game_states/game/game.asm
+++ game/logic/game_states/game/game.asm
@@ -1,3 +1,7 @@
+#include "game/logic/game_states/game/game_logic.asm"
+#include "game/logic/game_states/game/game_mode_local.asm"
+#include "game/logic/game_states/game/game_mode_online.asm"
+
 game_modes_init_lsb:
 .byt <game_mode_local_init
 .byt <game_mode_online_init
@@ -13,7 +17,3 @@ game_modes_pre_update_lsb:
 game_modes_pre_update_msb:
 .byt >game_mode_local_pre_update
 .byt >game_mode_online_pre_update
-
-#include "game/logic/game_states/game/game_logic.asm"
-#include "game/logic/game_states/game/game_mode_local.asm"
-#include "game/logic/game_states/game/game_mode_online.asm"
--- game/logic/game_states/game/game_mode_online.asm
+++ game/logic/game_states/game/game_mode_online.asm
@@ -1,22 +1,2 @@
-game_mode_online_init:
-.(
-#ifdef NETWORK_AI
-	jsr game_mode_local_init
-#endif
-
-	jmp network_init_stage
-	;rts ; useless - jump to a subroutine
-.)
-
-game_mode_online_pre_update:
-.(
-#ifdef NETWORK_AI
-	; Process AI, done before network call to override "physical" gamepad state
-	jsr game_mode_local_pre_update
-	lda controller_b_btns
-	sta controller_a_btns
-#endif
-
-	jmp network_tick_ingame
-	;rts ; useless - jump to a subroutine
-.)
+game_mode_online_init = network_init_stage
+game_mode_online_pre_update = network_tick_ingame
