fe29b502b9ded69a8ded79c0b42346b5f6cc154d
Sylvain Gadrat <sgadrat@wontfix.it>
2022-09-23 13:43:31 +0200

    credits: Add Issa and CasualCart
    Respectively for the idea of fighting on a crashing meteor, and for drawing the VGSage

--- game/data/menu_credits/credits.asm
+++ game/data/menu_credits/credits.asm
@@ -2,20 +2,45 @@ MENU_CREDITS_CREDITS_BANK = CURRENT_BANK_NUMBER
 
 .(
 &menu_credits_pages_illustration_lsb:
-	.byt <menu_credits_illustration_graphics,       <menu_credits_illustration_music,          <menu_credits_illustration_characters
-	.byt <menu_credits_illustration_special_thanks, <menu_credits_illustration_special_thanks, <menu_credits_illustration_author
+	.byt <menu_credits_illustration_graphics
+	.byt <menu_credits_illustration_music
+	.byt <menu_credits_illustration_characters
+	.byt <menu_credits_illustration_characters
+	.byt <menu_credits_illustration_special_thanks
+	.byt <menu_credits_illustration_special_thanks
+	.byt <menu_credits_illustration_author
 &menu_credits_pages_illustration_msb:
-	.byt >menu_credits_illustration_graphics,       >menu_credits_illustration_music,          >menu_credits_illustration_characters
-	.byt >menu_credits_illustration_special_thanks, >menu_credits_illustration_special_thanks, >menu_credits_illustration_author
+	.byt >menu_credits_illustration_graphics
+	.byt >menu_credits_illustration_music
+	.byt >menu_credits_illustration_characters
+	.byt >menu_credits_illustration_characters
+	.byt >menu_credits_illustration_special_thanks
+	.byt >menu_credits_illustration_special_thanks
+	.byt >menu_credits_illustration_author
 &menu_credits_pages_illustration_bank:
-	.byt MENU_CREDITS_ILLUSTRATION_GRAPHICS_BANK_NUMBER,       MENU_CREDITS_ILLUSTRATION_MUSIC_BANK_NUMBER,          MENU_CREDITS_ILLUSTRATION_CHARACTERS_BANK_NUMBER
-	.byt MENU_CREDITS_ILLUSTRATION_SPECIAL_THANKS_BANK_NUMBER, MENU_CREDITS_ILLUSTRATION_SPECIAL_THANKS_BANK_NUMBER, MENU_CREDITS_ILLUSTRATION_AUTHOR_BANK_NUMBER
+	.byt MENU_CREDITS_ILLUSTRATION_GRAPHICS_BANK_NUMBER
+	.byt MENU_CREDITS_ILLUSTRATION_MUSIC_BANK_NUMBER
+	.byt MENU_CREDITS_ILLUSTRATION_CHARACTERS_BANK_NUMBER
+	.byt MENU_CREDITS_ILLUSTRATION_CHARACTERS_BANK_NUMBER
+	.byt MENU_CREDITS_ILLUSTRATION_SPECIAL_THANKS_BANK_NUMBER
+	.byt MENU_CREDITS_ILLUSTRATION_SPECIAL_THANKS_BANK_NUMBER
+	.byt MENU_CREDITS_ILLUSTRATION_AUTHOR_BANK_NUMBER
 &menu_credits_pages_text_lsb:
-	.byt <text_graphics,        <text_musics,          <text_characters
-	.byt <text_special_thanks1, <text_special_thanks2, <text_author
+	.byt <text_graphics
+	.byt <text_musics
+	.byt <text_characters1
+	.byt <text_characters2
+	.byt <text_special_thanks1
+	.byt <text_special_thanks2
+	.byt <text_author
 &menu_credits_pages_text_msb:
-	.byt >text_graphics,        >text_musics,          >text_characters
-	.byt >text_special_thanks1, >text_special_thanks2, >text_author
+	.byt >text_graphics
+	.byt >text_musics
+	.byt >text_characters1
+	.byt >text_characters2
+	.byt >text_special_thanks1
+	.byt >text_special_thanks2
+	.byt >text_author
 pages_end:
 
 &MENU_CREDITS_NB_PAGES = pages_end-menu_credits_pages_text_msb
@@ -24,13 +49,13 @@ text_graphics:
 	.byt "   <GRAPHICS>   "
 	.byt "                "
 	.byt "                "
-	.byt "                "
 	.byt "      Fei       "
 	.byt "                "
 	.byt "Margarita Gadrat"
 	.byt "                "
 	.byt "Martin Le Borgne"
 	.byt "                "
+	.byt "  Casual Cart   "
 	.byt "                "
 	.byt "                "
 	.byt "                "
@@ -50,8 +75,9 @@ text_musics:
 	.byt "Super Tilt Bro. "
 	.byt "    #By Tui     "
 
-text_characters:
+text_characters1:
 	.byt "  <CHARACTERS>  "
+	.byt "                "
 	.byt "                "
 	.byt "      KIKI      "
 	.byt "   from Krita   "
@@ -61,12 +87,27 @@ text_characters:
 	.byt "f. Pepper&Carrot"
 	.byt " By David Revoy "
 	.byt "                "
+	.byt "                "
+	.byt "                "
+
+text_characters2:
+	.byt "  <CHARACTERS>  "
+	.byt "                "
+	.byt "                "
 	.byt "     SINBAD     "
 	.byt "   from Ogre3D  "
 	.byt "    By Zi Ye    "
+	.byt "                "
+	.byt "     VGSAGE     "
+	.byt "f. VideoGameSage"
+	.byt "  By VGS Staff  "
+	.byt "                "
+	.byt "                "
+	.byt "                "
 
 text_special_thanks1:
 	.byt "<SPECIAL THANKS>"
+	.byt "                "
 	.byt "                "
 	.byt "  Antoine Gohin "
 	.byt "                "
@@ -76,24 +117,23 @@ text_special_thanks1:
 	.byt "                "
 	.byt "    Bjorn Nah   "
 	.byt "                "
-	.byt "       Fei      "
-	.byt "                "
-	.byt "                "
+	.byt "     Dennis     "
+	.byt "  van den Broek "
 
 text_special_thanks2:
 	.byt "<SPECIAL THANKS>"
 	.byt "                "
-	.byt "     Dennis     "
-	.byt "  van den Broek "
+	.byt "       Fei      "
+	.byt "                "
+	.byt "      Issa      "
 	.byt "                "
 	.byt "  Keenan Hecht  "
 	.byt "                "
 	.byt "Margarita Gadrat"
 	.byt "                "
-	.byt "  supergameland "
+	.byt "  SuperGameLand "
 	.byt "                "
 	.byt "      Tui       "
-	.byt "                "
 
 text_author:
 	.byt "    <AUTHOR>    "
@@ -101,8 +141,8 @@ text_author:
 	.byt "                "
 	.byt "                "
 	.byt "                "
-	.byt "  Sylvain Gadrat"
-	.byt "  (Roger Bidon) "
+	.byt " Sylvain Gadrat "
+	.byt " (Roger Bidon)  "
 	.byt "                "
 	.byt "                "
 	.byt "                "
