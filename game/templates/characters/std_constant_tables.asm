5187210b015e65837a0ec6104fcea176595dfe8a
Sylvain Gadrat <sgadrat@wontfix.it>
2022-12-04 13:28:22 +0100

    sinbad: Increase ground friction

--- game/templates/characters/std_constant_tables.asm
+++ game/templates/characters/std_constant_tables.asm
@@ -5,7 +5,11 @@ acceleration_table({char_name_upper}_AIR_FRICTION_STRENGTH, {char_name}_air_fric
 velocity_table({char_name_upper}_FASTFALL_SPEED, {char_name}_fastfall_speed_msb, {char_name}_fastfall_speed_lsb)
 acceleration_table({char_name_upper}_GROUND_FRICTION_STRENGTH, {char_name}_ground_friction_strength)
 acceleration_table({char_name_upper}_GROUND_FRICTION_STRENGTH/3, {char_name}_ground_friction_strength_weak)
+#if {char_name_upper}_GROUND_FRICTION_STRENGTH*3 <= $ff
 acceleration_table({char_name_upper}_GROUND_FRICTION_STRENGTH*3, {char_name}_ground_friction_strength_strong)
+#else
+acceleration_table($ff, {char_name}_ground_friction_strength_strong)
+#endif
 velocity_table({char_name_upper}_TECH_SPEED, {char_name}_tech_speed_msb, {char_name}_tech_speed_lsb)
 velocity_table(-{char_name_upper}_TECH_SPEED, {char_name}_tech_speed_neg_msb, {char_name}_tech_speed_neg_lsb)
 velocity_table(-{char_name_upper}_JUMP_POWER, {char_name}_jump_velocity_msb, {char_name}_jump_velocity_lsb)
