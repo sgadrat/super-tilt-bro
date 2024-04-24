473d3623656535467b942f99a99bdba0b40a3299
Sylvain Gadrat <sgadrat@wontfix.it>
2024-04-24 22:15:54 +0200

    online menu: fix wrap_esp_wait_ready being optimized out of the loop

--- game/cstb/asm_routines.h
+++ game/cstb/asm_routines.h
@@ -482,7 +482,7 @@ static void wrap_start_rendering(uint8_t scroll_nametable) {
 
 static bool wrap_esp_wait_ready() {
 	uint8_t res;
-	asm(
+	asm volatile(
 		"jsr esp_wait_ready\n\t"
 		"sta %0"
 		: "=rm"(res)
